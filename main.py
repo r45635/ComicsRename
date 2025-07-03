4cc42019d9c5e65fa1a2e6da7a47553041ee2c5f
Vincent <r45635@users.noreply.github.com>
2025-07-03 15:49:26 -0700

    Bump version to 3.4.1 for new release

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
         # Create the QApplication
         app = QApplication(sys.argv)
         app.setApplicationName("ComicsRename")
-        app.setApplicationVersion("3.4.0")
+        app.setApplicationVersion("3.4.1")
         app.setOrganizationName("ComicsRename")
         app.setOrganizationDomain("github.com")
         
